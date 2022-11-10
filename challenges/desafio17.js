1adb1f13ea4929be0a26a9bdbbdd4aeb7aefb271
Fernanda Vidal <fevidal.dev@gmail.com>
2022-11-09 21:26:18 -0300

    feat: requisito 17.

---
+++ challenges/desafio17.js
@@ -0,0 +1,5 @@
+db.resumoProdutos.insertOne(
+  { franquia: "McDonalds", totalProdutos: db.produtos.countDocuments({}) },
+);
+
+db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
